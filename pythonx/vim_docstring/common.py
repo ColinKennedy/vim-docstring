9a28e468cccecdd854e429b67b5d00ffc3bdb3e8
Colin Kennedy <colinvfx@gmail.com>
2019-01-01 13:27:17 -0500

    common.py - Added missing docstrings

--- pythonx/vim_docstring/common.py
+++ pythonx/vim_docstring/common.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''A series of commands for finding and getting different types of Vim folds.'''
+'''A series of commands for finding and getting different types of Vim folds.
+
+Note:
+    In Vim, the first line of a buffer's index is 1, not 0. There's a few
+    points in this file where "1-based" is mentioned. That's what it means.
+
+'''
 
 # IMPORT STANDARD LIBRARIES
 import ast
@@ -20,12 +26,26 @@ def is_fold_open(line):
 
 
 def get_current_buffer_root_node():
-    '''tuple[`ast.Node`, list[str]]: Get the module-level ast node and its lines.'''
+    '''tuple[`ast.Module`, list[str]]: Get the ast node and its lines.'''
     lines = list(vim.current.buffer)
     return (ast.parse("\n".join(lines)), lines)
 
 
 def get_node_folds(root, lines):
+    '''Find every fold in the given node.
+
+    Args:
+        root (`ast.Node`):
+            The node which will be searched for child nodes which should have docstrings.
+        lines (list[str]):
+            The source-code lines that `root` represents.
+
+    Returns:
+        tuple[`ast.ClassDef` or `ast.FunctionDef` or `ast.Module`, int, int]:
+            Every child node of `root` and the start row and end row of its
+            docstring. The start and end rows are 1-based.
+
+    '''
     folds = []
 
     for node in get_valid_nodes(root):
@@ -58,6 +78,7 @@ def get_node_folds(root, lines):
 
 
 def get_open_folds():
+    '''list[`ast.Node`]: Find every node that has a docstring which is folded.'''
     root, lines = get_current_buffer_root_node()
 
     folds = []
@@ -69,10 +90,20 @@ def get_open_folds():
 
 
 def get_valid_nodes(root):
+    '''Find every node that would have a Python docstring.
+
+    Args:
+        root (`ast.Node`):
+            The node which will be searched for child nodes which should have docstrings.
+
+    Returns:
+        list[`ast.ClassDef` or `ast.FunctionDef` or `ast.Module`]: The found nodes.
+
+    '''
     nodes = []
 
     for node in ast.walk(root):
-        if not isinstance(node, (ast.Module, ast.FunctionDef, ast.ClassDef)):
+        if not isinstance(node, (ast.ClassDef, ast.FunctionDef, ast.Module)):
             continue
         if not ast.get_docstring(node):
             continue
