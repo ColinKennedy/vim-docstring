6189939da62219a1db02813083ccfdbbd7201055
Colin Kennedy <colinvfx@gmail.com>
2019-01-01 13:36:05 -0500

    vim_docstring_python.py - Added missing docstrings

--- pythonx/vim_docstring/vim_docstring_python.py
+++ pythonx/vim_docstring/vim_docstring_python.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''The main module that is responsible for creating and opening folds.'''
+
 # IMPORT THIRD-PARTY LIBRARIES
 import vim
 
@@ -12,6 +14,7 @@ DEFAULT_VARIABLE = 'b:vim_docstring_open_folds'
 
 
 def set_folds():
+    '''Create folds for every Python docstring in the current buffer.'''
     root, lines = common.get_current_buffer_root_node()
 
     for _, start, end in common.get_node_folds(root, lines):
@@ -19,12 +22,14 @@ def set_folds():
 
 
 def save_opened_folds(variable=DEFAULT_VARIABLE):
+    '''Save a list of every node with an open fold to the given Vim `variable`.'''
     nodes = common.get_open_folds()
     names = [node.name for node in nodes]
     vim.command('let {variable} = {names!r}'.format(variable=variable, names=names))
 
 
 def restore_opened_folds(variable=DEFAULT_VARIABLE):
+    '''Find every saved, open Python docstring fold and open them.'''
     try:
         opened_folds = vim.eval(variable)
     except Exception:
@@ -33,5 +38,5 @@ def restore_opened_folds(variable=DEFAULT_VARIABLE):
 
     root, lines = common.get_current_buffer_root_node()
     for node, start, end in common.get_node_folds(root, lines):
-        if node.name in opened_folds:
+        if node.name in opened_folds and not common.is_fold_open(start):
             vim.command('{start},{end}foldopen'.format(start=start, end=end))
